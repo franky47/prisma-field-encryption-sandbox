e76ee9825ae15843c8573f226126a684c7ae308f
Francois Best <francois@francoisbest.com>
2021-11-25 22:17:07 +0100

    feat: Using beta middleware & nested queries

--- script.ts
+++ script.ts
@@ -1,56 +1,58 @@
-import { PrismaClient } from "@prisma/client";
-import { fieldEncryptionMiddleware } from "./src/middlewares/fieldEncryption";
+import { PrismaClient } from '@prisma/client'
+import { fieldEncryptionMiddleware } from 'prisma-field-encryption'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 prisma.$use(
   fieldEncryptionMiddleware({
     fields: {
       // List what you want to encrypt as "{Model}.{field}": true
-      "Post.content": true,
-      "User.name": true,
+      'Post.content': true,
+      'User.name': true,
     },
     // Generate keys: https://cloak.47ng.com
-    encryptionKey: "k1.aesgcm256.OsqVmAOZBB_WW3073q1wU4ag0ap0ETYAYMh041RuxuI=",
+    encryptionKey: 'k1.aesgcm256.OsqVmAOZBB_WW3073q1wU4ag0ap0ETYAYMh041RuxuI=',
   })
-);
+)
 
 async function main() {
-  // Play with the Prisma API here.
-  // Supported operations on models with encrypted fields:
-  // - create, update, upsert
-  // - findUnique, findFirst
-  // - delete
-  // Note: extending results with `include` is not yet supported
-  // (included models will show up encrypted)
+  // Play with the Prisma API here, and report back:
+  // https://github.com/47ng/prisma-field-encryption/issues/new
+  //
+  // Caveats & Limitations: you cannot filter on encrypted fields, eg:
+  // prisma.user.findUnique({ where: { name: 'Super secret' }}) won't work.
+  // Also, direct SQL operations with $execute and $raw are not supported.
+
+  const USER_EMAIL = 'secret.spy@cia.gov'
 
   // Clean slate
   try {
-    process.env.PRISMA_FIELD_ENCRYPTION_LOG = "false";
-    await prisma.user.delete({ where: { email: "secret.spy@cia.gov" } });
+    process.env.PRISMA_FIELD_ENCRYPTION_LOG = 'false'
+    await prisma.user.delete({ where: { email: USER_EMAIL } })
+    await prisma.post.deleteMany()
   } catch {}
 
   // Un-comment this line to see internal operations:
-  process.env.PRISMA_FIELD_ENCRYPTION_LOG = "true";
+  // process.env.PRISMA_FIELD_ENCRYPTION_LOG = 'true'
 
   await prisma.user.create({
     data: {
-      email: "secret.spy@cia.gov",
-      name: "Super secret",
+      email: USER_EMAIL,
+      name: 'Super secret',
     },
-  });
+  })
   const superSecretSpy = await prisma.user.findUnique({
-    where: { email: "secret.spy@cia.gov" },
-  });
+    where: { email: USER_EMAIL },
+  })
   await prisma.user.update({
-    where: { email: "secret.spy@cia.gov" },
+    where: { email: USER_EMAIL },
     data: {
-      name: "Under cover",
+      name: 'Under cover',
     },
-  });
+  })
   const underCoverSpy = await prisma.user.findFirst({
-    where: { email: "secret.spy@cia.gov" },
-  });
+    where: { email: USER_EMAIL },
+  })
 
   const report = await prisma.post.create({
     data: {
@@ -59,25 +61,59 @@ async function main() {
           email: underCoverSpy?.email,
         },
       },
-      title: "Secret report",
-      content: "I have infiltrated the enemy base.",
+      title: 'Secret report',
+      content: 'I have infiltrated the enemy base.',
     },
-  });
+  })
+
+  const userUpdate = await prisma.user.update({
+    where: { email: USER_EMAIL },
+    data: {
+      name: 'I am the enemy now',
+      posts: {
+        update: {
+          where: {
+            id: report.id,
+          },
+          data: {
+            content: 'I hereby resign from my position, effective immediately.',
+          },
+        },
+      },
+    },
+    include: {
+      posts: {
+        select: {
+          title: true,
+          content: true,
+        },
+      },
+    },
+  })
+
+  const usersAndTheirPosts = await prisma.user.findMany({
+    include: {
+      posts: {
+        select: {
+          title: true,
+          content: true,
+        },
+      },
+    },
+  })
 
   console.dir(
     {
       superSecretSpy,
       underCoverSpy,
       report,
+      userUpdate,
+      usersAndTheirPosts,
     },
     { depth: Infinity }
-  );
+  )
 }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main().finally(async () => {
+  await prisma.$disconnect()
+})
